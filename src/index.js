509e599c00a9fb8b237e4e01ad7e7528506e6516
Ephraim Solomon <efisolm22@gmail.com>
2024-02-26 15:49:01 +0300

    DB connection intialized

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const app = require('./app');
+const dotenv = ('dotenv/config'); // This loads the .env file into process.env
+const { createConnection } = require('typeorm');
 
+async function connectToDatabase() {
+  try {
+      await createConnection({
+          type: 'mysql',
+          host: process.env.DB_HOST,
+          port: parseInt(process.env.DB_PORT, 10),
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE,
+          entities: [
+              // Add your entity classes here
+          ],
+          synchronize: true, // Set to false in production
+      });
+      console.log('Connected to the database');
+  } catch (error) {
+      console.error('Error connecting to the database:', error);
+  }
+}
+connectToDatabase();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} and can be accessed http://localhost:${PORT}`);
